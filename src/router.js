9565f01edd83da18c1e12f1c6b1ca4e62a9282d8
Iwan Sutandi <sutandi@student.spb.ac.id>
2019-10-21 13:16:00 +0800

    try fix pwa with routes lazy component

--- src/router.js
+++ src/router.js
@@ -1,17 +1,16 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Home from './views/Home.vue'
+// import Home from './views/Home.vue'
 
 Vue.use(Router)
 
 export default new Router({
   mode: 'history',
-  base: __dirname,
   routes: [
     {
       path: '/',
       name: 'home',
-      component: Home
+      component: () => import('./views/Home.vue')
     },
     {
       path: '/about',
