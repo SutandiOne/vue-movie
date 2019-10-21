fc01d9566b9c8b56af3115855e54480e3cd99b8b
Iwan Sutandi <sutandi@student.spb.ac.id>
2019-10-21 13:31:29 +0800

    try fix pwa in  router.js to add alias

--- src/router.js
+++ src/router.js
@@ -1,16 +1,21 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-// import Home from './views/Home.vue'
+import Home from './views/Home.vue'
 
 Vue.use(Router)
 
 export default new Router({
   mode: 'history',
   routes: [
+    {   
+      path: '/index.html',
+      component: Home,
+      alias: '/'
+    },
     {
       path: '/',
       name: 'home',
-      component: () => import('./views/Home.vue')
+      component: Home
     },
     {
       path: '/about',
